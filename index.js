e24044091aecfc037793b475a7daf0ce91adf7ed
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2021-12-24 10:44:36 +0000

    added function to add css animation on btn click

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const btn = document.getElementById("btn");
 // Task:
 // - Write a function to launch the sleigh!
 // - See CSS for more tasks.
+function launch() {
+  crew.classList.add("launch");
+}
+
+btn.addEventListener("click", launch);
+
 // Stretch goals:
 // - Add a cheering sound when the sleigh takes off.
 // - Add a countdown to the launch time.
