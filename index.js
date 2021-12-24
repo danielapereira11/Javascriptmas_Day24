17f96005bc72e4576ea66afdda61f0d8818cfda0
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2021-12-24 12:46:30 +0000

    Added countdown before launch

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const crew = document.getElementById("crew");
 const btn = document.getElementById("btn");
+const countdownEl = document.querySelector("#countdown");
 
-// Task:
-// - Write a function to launch the sleigh!
-// - See CSS for more tasks.
 function launch() {
   crew.classList.add("launch");
 }
 
-btn.addEventListener("click", launch);
+function countingdown() {
+  let count = 3;
+  let countdown = setInterval(function () {
+    if (count === 0) {
+      clearInterval(countdown);
+      countdownEl.innerHTML = `<p class="ho-ho-ho">HO! HO! HO!</p>`;
+      launch();
+    } else {
+      countdownEl.innerHTML = `<p class="countdown">${count}</p>`;
+      count--;
+    }
+  }, 1000);
+}
+
+btn.addEventListener("click", countingdown);
+
+// Task:
+// - Write a function to launch the sleigh!
+// - See CSS for more tasks.
 
 // Stretch goals:
 // - Add a cheering sound when the sleigh takes off.
